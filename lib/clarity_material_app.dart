36f4d472ca25cc0c3a0c4e7e2baf36810684add4
Mohammed Elshora <elshoram974@gmail.com>
2023-12-11 19:39:41 +0200

    refactor code of localeResolutionCallback

--- lib/clarity_material_app.dart
+++ lib/clarity_material_app.dart
@@ -1,8 +1,6 @@
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
 import 'package:data_sharing_organizing/core/utils/config/locale/locale_handler.dart';
 import 'package:data_sharing_organizing/core/utils/config/themes/app_theme.dart';
-import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
-import 'package:data_sharing_organizing/core/utils/services/dependency.dart';
 import 'package:data_sharing_organizing/features/splash/presentation/cubit/config_cubit.dart';
 import 'package:data_sharing_organizing/routes.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +12,7 @@ class ClarityApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ConfigCubit cubit =
-        BlocProvider.of<ConfigCubit>(context, listen: true);
+    final ConfigCubit c = BlocProvider.of<ConfigCubit>(context, listen: true);
     return MaterialApp.router(
       localizationsDelegates: const [
         S.delegate,
@@ -24,22 +21,13 @@ class ClarityApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      locale: cubit.appLocale,
+      locale: c.appLocale,
       title: S.current.sharikna,
       routerConfig: AppRoute.router,
       theme: AppTheme.lightMode,
       darkTheme: AppTheme.darkMode,
-      themeMode: cubit.themeMode,
-      localeResolutionCallback: (locale, supportedLocales) {
-        if (locale == null || pref.containsKey(AppStrings.locale)) return null;
-        final Locale deviceLocale = AppLocale().deviceLocale;
-        final bool isSupported = S.delegate.isSupported(deviceLocale);
-        if (isSupported) {
-          cubit.appLocale = deviceLocale;
-          return deviceLocale;
-        }
-        return null;
-      },
+      themeMode: c.themeMode,
+      localeResolutionCallback: (l, _) => AppLocale.localeResolutionCallback(l, c),
     );
   }
 }
--- lib/core/utils/config/locale/locale_handler.dart
+++ lib/core/utils/config/locale/locale_handler.dart
@@ -1,9 +1,24 @@
 import 'dart:ui';
 
+import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../features/splash/presentation/cubit/config_cubit.dart';
+import 'generated/l10n.dart';
 
 final class AppLocale {
   static final PlatformDispatcher dispatcher = WidgetsBinding.instance.platformDispatcher;
    final Locale deviceLocale = dispatcher.locale;
    final List<Locale> systemLocales = dispatcher.locales;
+  static Locale? localeResolutionCallback(Locale? locale, ConfigCubit cubit) {
+  if (locale == null || pref.containsKey(AppStrings.locale)) return null;
+  final Locale deviceLocale = AppLocale().deviceLocale;
+  final bool isSupported = S.delegate.isSupported(deviceLocale);
+  if (isSupported) {
+    cubit.appLocale = deviceLocale;
+    return deviceLocale;
+  }
+  return null;
+}
 }
