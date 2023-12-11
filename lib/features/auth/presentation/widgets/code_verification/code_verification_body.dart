3b82a10a3205a1c7cd2704c22de911e0ededd842
Mohammed Elshora <elshoram974@gmail.com>
2023-12-11 23:37:07 +0200

    end code verification body ui

---
+++ lib/features/auth/presentation/widgets/code_verification/code_verification_body.dart
@@ -0,0 +1,22 @@
+import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:flutter/material.dart';
+
+import '../auth_body.dart';
+import 'code_fields.dart';
+import 'code_verification_buttons.dart';
+
+class CodeVerificationBody extends StatelessWidget {
+  const CodeVerificationBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthBody(
+      introHeader: S.of(context).codeVerification,
+      introBody: S.of(context).enterDigitCodeThatHasBeenSentToYourEmail,
+      children: const [
+        CodeFields(),
+        CodeVerificationButtons(),
+      ],
+    );
+  }
+}
