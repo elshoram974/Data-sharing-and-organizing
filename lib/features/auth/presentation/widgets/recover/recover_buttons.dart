9dbe1ee7ced04e41265eb02e6d6e203fcaf6ee02
Mohammed Elshora <elshoram974@gmail.com>
2023-12-11 21:47:49 +0200

    recover buttons widget

---
+++ lib/features/auth/presentation/widgets/recover/recover_buttons.dart
@@ -0,0 +1,35 @@
+import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../auth_filled_button.dart';
+import '../auth_rich_button.dart';
+
+class RecoverButtons extends StatelessWidget {
+  const RecoverButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(height: 53),
+        AuthFilledButton(
+          text: S.of(context).sendRequest,
+          onPressed: () {},
+        ),
+        const SizedBox(height: 8),
+        AuthRichButton(
+          prefix: Icon(
+            Icons.arrow_back_outlined,
+            color: AppStyle.styleRegular15.color,
+          ),
+          prefixText: S.of(context).noNeed,
+          buttonText: S.of(context).goBack,
+          onTap: context.pop,
+        )
+      ],
+    );
+  }
+}
