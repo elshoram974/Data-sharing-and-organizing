73c81d5e671328b3c32ec3586e10585f65fd85f0
Mohammed Elshora <elshoram974@gmail.com>
2023-12-11 21:29:42 +0200

    AuthRichButton

---
+++ lib/features/auth/presentation/widgets/auth_rich_button.dart
@@ -0,0 +1,46 @@
+import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
+import 'package:data_sharing_organizing/core/utils/styles.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class AuthRichButton extends StatelessWidget {
+  const AuthRichButton({
+    super.key,
+    this.prefix,
+    required this.prefixText,
+    required this.buttonText,
+    this.onTap,
+  });
+  final Widget? prefix;
+  final String prefixText;
+  final String buttonText;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 25),
+      child: RichText(
+        textAlign: TextAlign.end,
+        text: TextSpan(
+          style: AppStyle.styleRegular15,
+          children: [
+            if (prefix != null)
+              WidgetSpan(
+                alignment: PlaceholderAlignment.middle,
+                child: prefix!,
+              ),
+            TextSpan(
+              text: prefixText,
+            ),
+            TextSpan(
+              style: const TextStyle(color: AppColor.active),
+              text: buttonText,
+              recognizer: TapGestureRecognizer()..onTap = onTap,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/widgets/login/recover_account_widget.dart
+++ lib/features/auth/presentation/widgets/login/recover_account_widget.dart
@@ -1,28 +1,19 @@
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
-import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
-import 'package:data_sharing_organizing/core/utils/styles.dart';
+import 'package:data_sharing_organizing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../auth_rich_button.dart';
 
 class RecoverAccountButton extends StatelessWidget {
   const RecoverAccountButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 25),
-      child: RichText(
-        textAlign: TextAlign.end,
-        text: TextSpan(
-          style: AppStyle.styleRegular15,
-          text: S.of(context).forgetPassword,
-          children: [
-            TextSpan(
-              style: const TextStyle(color: AppColor.active),
-              text: S.of(context).recover,
-            )
-          ],
-        ),
-      ),
+    return AuthRichButton(
+      prefixText: S.of(context).forgetPassword,
+      buttonText: S.of(context).recover,
+      onTap: () => context.push(AppRoute.recoverScreen),
     );
   }
 }
