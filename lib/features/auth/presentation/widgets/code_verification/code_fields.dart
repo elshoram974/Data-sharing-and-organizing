73cd2c95ef760df726b44db05a164a82b8cbb913
Mohammed Elshora <elshoram974@gmail.com>
2023-12-13 10:50:13 +0200

    Pinput contain digits only

--- lib/features/auth/presentation/widgets/code_verification/code_fields.dart
+++ lib/features/auth/presentation/widgets/code_verification/code_fields.dart
@@ -1,6 +1,7 @@
 import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
 import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pinput/pinput.dart';
 
 class CodeFields extends StatelessWidget {
@@ -10,22 +11,21 @@ class CodeFields extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 57),
-      child: Form(
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: Pinput(
-            length: 6,
-            autofocus: true,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            defaultPinTheme: PinTheme(
-              height: 48,
-              width: 54,
-              textStyle: const TextStyle(color: Colors.black),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: AppStyle.styleRegular15.color!),
-                borderRadius: BorderRadius.circular(AppConst.borderRadius),
-              ),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Pinput(
+          length: 6,
+          autofocus: true,
+          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          defaultPinTheme: PinTheme(
+            height: 48,
+            width: 54,
+            textStyle: const TextStyle(color: Colors.black),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: AppStyle.styleRegular15.color!),
+              borderRadius: BorderRadius.circular(AppConst.borderRadius),
             ),
           ),
         ),
