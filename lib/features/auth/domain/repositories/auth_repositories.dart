23faeb3fd32ec7f22c74745a955db71a79faf161
Mohammed Elshora <elshoram974@gmail.com>
2023-12-15 10:20:53 +0200

    logOut,isLoggedIn domain repo

--- lib/features/auth/domain/repositories/auth_repositories.dart
+++ lib/features/auth/domain/repositories/auth_repositories.dart
@@ -4,6 +4,8 @@ import '../entities/auth_user_entity.dart';
 
 abstract class AuthRepositories {
   Future<Status<AuthUserEntity>> login(AuthUserEntity user);
+  Future<Status<int>> logOut();
+  Status<bool> isLoggedIn();
   Future<Status<AuthUserEntity>> signUp(AuthUserEntity user);
   Future<Status<AuthUserEntity>> requestToRecoverAccount(String email);
   Future<Status<AuthUserEntity>> verifyCode(int code);
