054441a13b503250187ed68b1f90bac8fbc8b05e
Mohammed Elshora <elshoram974@gmail.com>
2023-12-20 17:28:39 +0200

    MyBlocObserver create - close

--- lib/core/utils/services/bloc_observer.dart
+++ lib/core/utils/services/bloc_observer.dart
@@ -4,8 +4,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyBlocObserver extends BlocObserver {
   @override
+  void onCreate(BlocBase bloc) {
+    log('create: ${bloc.toString()}');
+    super.onCreate(bloc);
+  }
+
+  @override
   void onChange(BlocBase bloc, Change change) {
     log('Change: ${change.toString()}');
     super.onChange(bloc, change);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    log('close: ${bloc.toString()}');
+    super.onClose(bloc);
   }
 }
