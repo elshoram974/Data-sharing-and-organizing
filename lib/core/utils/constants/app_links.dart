94553aa8718e48318d5feb980e661846908bc9ad
Mohammed Elshora <elshoram974@gmail.com>
2023-12-24 23:39:04 +0200

    login link + base link

--- lib/core/utils/constants/app_links.dart
+++ lib/core/utils/constants/app_links.dart
@@ -2,4 +2,10 @@ abstract final class AppLinks {
   const AppLinks();
 
   static const String appLink = '';
+
+  static const String _baseLink = 'https://thiet.mrecode.com/api';
+
+  // * authorization links
+  static const String _auth = '$_baseLink/auth/users';
+  static const String login = '$_auth/login_user.php';
 }
