74d727649108a5a86dece6be76617c97ec54a3d4
Mohammed Elshora <elshoram974@gmail.com>
2024-01-24 19:01:04 +0200

    end all login with social accounts

--- lib/core/utils/services/social_services.dart
+++ lib/core/utils/services/social_services.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
 import 'package:data_sharing_organizing/features/auth/domain/entities/auth_user_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
@@ -10,6 +11,28 @@ final class SocialServices {
 
   // * google sign in
   Future<AuthUserEntity?> signInWithGoogle() async {
+    late final UserCredential? user;
+    if (AppConst.isWeb) {
+      user = await _getGoogleUserInWeb();
+    } else {
+      user = await _getGoogleUserInAndroid();
+    }
+
+    log('sign in with google : ${user?.user}');
+    return _convertToAuthEntity(user);
+  }
+
+  Future<UserCredential?> _getGoogleUserInWeb() {
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    googleProvider
+        .addScope('https://www.googleapis.com/auth/contacts.readonly');
+    googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+    return FirebaseAuth.instance.signInWithPopup(googleProvider);
+  }
+
+  Future<UserCredential?> _getGoogleUserInAndroid() async {
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
     if (gUser == null) return null;
 
@@ -20,33 +43,53 @@ final class SocialServices {
       idToken: gAuth.idToken,
     );
 
-    UserCredential user = await FirebaseAuth.instance.signInWithCredential(credential);
-
-    log('sign in with google :${user.user}');
-    if (user.user == null) return null;
-    
-    final AuthUserEntity auth = AuthUserEntity.fromFirebaseAuth(user.user!);
-    
-    if (user.user!.email == null) return auth.copyWith(email: user.user!.uid);
-
-    return auth;
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
+
+  // ------------------------------------------
 
   // * face sign in
   Future<AuthUserEntity?> signInWithFacebook() async {
+    late final UserCredential? user;
+    if (AppConst.isWeb) {
+      user = await _getFacebookUserInWeb();
+    } else {
+      user = await _getFacebookUserInAndroid();
+    }
+
+    log('sign in with facebook : ${user?.user}');
+
+    return _convertToAuthEntity(user);
+  }
+
+  Future<UserCredential?> _getFacebookUserInWeb() async {
+    final FacebookAuthProvider facebookProvider = FacebookAuthProvider();
+
+    facebookProvider.addScope('email');
+    facebookProvider.setCustomParameters({
+      'display': 'popup',
+    });
+
+    return await FirebaseAuth.instance.signInWithPopup(facebookProvider);
+  }
+
+  Future<UserCredential?> _getFacebookUserInAndroid() async {
     final LoginResult loginResult = await FacebookAuth.instance.login();
     if (loginResult.status != LoginStatus.success) return null;
 
-    final OAuthCredential credential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    final OAuthCredential credential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-    UserCredential user = await FirebaseAuth.instance.signInWithCredential(credential);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
-    log('sign in with facebook :${user.user}');
+  // -----------------------------------------------
 
-    if (user.user == null) return null;
+  AuthUserEntity? _convertToAuthEntity(UserCredential? user) {
+    if (user?.user == null) return null;
 
-    final AuthUserEntity auth = AuthUserEntity.fromFirebaseAuth(user.user!);
-    
+    final AuthUserEntity auth = AuthUserEntity.fromFirebaseAuth(user!.user!);
+
     if (user.user!.email == null) return auth.copyWith(email: user.user!.uid);
 
     return auth;
