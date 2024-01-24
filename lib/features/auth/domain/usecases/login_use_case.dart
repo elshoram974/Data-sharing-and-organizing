08f59d98e5cecbc92d1b410c22918734b6d6e474
Mohammed Elshora <elshoram974@gmail.com>
2024-01-24 10:36:11 +0200

    make login with in usecase

--- lib/features/auth/domain/usecases/login_use_case.dart
+++ lib/features/auth/domain/usecases/login_use_case.dart
@@ -1,17 +1,31 @@
 import 'package:data_sharing_organizing/core/status/status.dart';
 import 'package:data_sharing_organizing/core/usecase/usecase_with_parameter.dart';
+import 'package:data_sharing_organizing/core/utils/enums/user_provider_enum.dart';
 
 import '../../data/models/app_user/user.dart';
 import '../entities/login_entity.dart';
 import '../repositories/auth_repositories.dart';
 
-final class LoginUseCase extends UseCase<User, LoginUserEntity> {
+final class LoginUseCase extends UseCase<User, ({UserProvider provider, LoginUserEntity user})> {
   final AuthRepositories authRepositories;
 
   LoginUseCase(this.authRepositories);
 
   @override
-  Future<Status<User>> call(LoginUserEntity param) {
-    return authRepositories.login(param);
+  Future<Status<User>> call(
+      ({UserProvider provider, LoginUserEntity user}) param) {
+    switch (param.provider) {
+      case UserProvider.emailPassword:
+        return authRepositories.login(param.user);
+
+      case UserProvider.google:
+        return authRepositories.loginWithGoogle(param.user);
+
+      case UserProvider.facebook:
+        return authRepositories.loginWithFacebook(param.user);
+
+      default:
+        return authRepositories.login(param.user);
+    }
   }
 }
