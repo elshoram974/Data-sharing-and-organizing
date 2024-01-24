f3787d99ac5c7139bd6c6183c1ce80fa67874d90
Mohammed Elshora <elshoram974@gmail.com>
2024-01-24 09:06:52 +0200

    add face and google logos to app

--- lib/core/utils/constants/app_assets.dart
+++ lib/core/utils/constants/app_assets.dart
@@ -7,4 +7,6 @@ abstract final class AppAssets {
   static const String _images = '$_assets/images';
   static const String logo = '$_images/logo.svg';
   static const String pathArrow = '$_images/path_arrow.svg';
+  static const String googleLogo = '$_images/google_logo.svg';
+  static const String facebookLogo = '$_images/facebook_logo.svg';
 }
