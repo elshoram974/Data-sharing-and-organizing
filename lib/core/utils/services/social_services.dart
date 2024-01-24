ffcaae42ce142a0279ba1391f6e2bdb7b31308c8
Mohammed Elshora <elshoram974@gmail.com>
2024-01-24 17:43:42 +0200

    facebook login

--- lib/core/utils/services/social_services.dart
+++ lib/core/utils/services/social_services.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:data_sharing_organizing/features/auth/domain/entities/auth_user_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final class SocialServices {
@@ -23,27 +24,31 @@ final class SocialServices {
 
     log('sign in with google :${user.user}');
     if (user.user == null) return null;
-    return AuthUserEntity.fromFirebaseAuth(user.user!);
+    
+    final AuthUserEntity auth = AuthUserEntity.fromFirebaseAuth(user.user!);
+    
+    if (user.user!.email == null) return auth.copyWith(email: user.user!.uid);
+
+    return auth;
   }
 
   // * face sign in
   Future<AuthUserEntity?> signInWithFacebook() async {
-    // final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
-    // if (gUser == null) return null;
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+    if (loginResult.status != LoginStatus.success) return null;
 
-    // final GoogleSignInAuthentication gAuth = await gUser.authentication;
+    final OAuthCredential credential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-    // final OAuthCredential credential =
-    //     GoogleAuthProvider.credential(
-    //   accessToken: gAuth.accessToken,
-    //   idToken: gAuth.idToken,
-    // );
+    UserCredential user = await FirebaseAuth.instance.signInWithCredential(credential);
 
-    // UserCredential user =
-    //     await FirebaseAuth.instance.signInWithCredential(credential);
+    log('sign in with facebook :${user.user}');
 
-    // log(user.user.toString());
-    // if (user.user == null) return null;
-    // return AuthUserEntity.fromFirebaseAuth(user.user!);
+    if (user.user == null) return null;
+
+    final AuthUserEntity auth = AuthUserEntity.fromFirebaseAuth(user.user!);
+    
+    if (user.user!.email == null) return auth.copyWith(email: user.user!.uid);
+
+    return auth;
   }
 }
