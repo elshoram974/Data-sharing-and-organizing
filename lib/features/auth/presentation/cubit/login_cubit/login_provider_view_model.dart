383a7065159cd8cf1ad84edc23f336cfb4e1320e
Mohammed Elshora <elshoram974@gmail.com>
2024-01-24 10:56:26 +0200

    end user provider view model

--- lib/features/auth/presentation/cubit/login_cubit/login_provider_view_model.dart
+++ lib/features/auth/presentation/cubit/login_cubit/login_provider_view_model.dart
@@ -1,14 +1,21 @@
 import 'package:data_sharing_organizing/core/utils/constants/app_assets.dart';
+import 'package:data_sharing_organizing/core/utils/enums/user_provider_enum.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 
 final class LoginProvidersViewModel {
   final String assetName;
   final void Function()? onTap;
 
   LoginProvidersViewModel({required this.assetName, required this.onTap});
-  
 
   static List<LoginProvidersViewModel> providers = [
-    LoginProvidersViewModel(assetName: AppAssets.facebookLogo, onTap: () {}),
-    LoginProvidersViewModel(assetName: AppAssets.googleLogo, onTap: () {}),
+    LoginProvidersViewModel(
+      assetName: AppAssets.facebookLogo,
+      onTap: () => ProviderDependency.login.socialLogin(UserProvider.facebook),
+    ),
+    LoginProvidersViewModel(
+      assetName: AppAssets.googleLogo,
+      onTap: () => ProviderDependency.login.socialLogin(UserProvider.google),
+    ),
   ];
 }
