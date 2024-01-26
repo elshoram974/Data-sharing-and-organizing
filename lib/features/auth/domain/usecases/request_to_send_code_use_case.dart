2367396777969ffe1e16acd428619671daec6abc
Mohammed Elshora <elshoram974@gmail.com>
2024-01-26 09:29:45 +0200

    end usecase after edit links

--- lib/features/auth/domain/usecases/request_to_send_code_use_case.dart
+++ lib/features/auth/domain/usecases/request_to_send_code_use_case.dart
@@ -1,16 +1,17 @@
 import 'package:data_sharing_organizing/core/status/status.dart';
 import 'package:data_sharing_organizing/core/usecase/usecase_with_parameter.dart';
+import 'package:data_sharing_organizing/core/utils/enums/verification_type_enum.dart';
 
 import '../../data/models/app_user/user.dart';
 import '../repositories/auth_repositories.dart';
 
-final class RequestToSendCodeUseCase extends UseCase<User, int> {
+final class RequestToSendCodeUseCase extends UseCase<User, ({String email, VerificationType verification})> {
   final AuthRepositories authRepositories;
 
   RequestToSendCodeUseCase(this.authRepositories);
 
   @override
-  Future<Status<User>> call(int param) {
+  Future<Status<User>> call(({String email, VerificationType verification}) param) {
     return authRepositories.requestToSendCode(param);
   }
 }
--- lib/features/auth/domain/usecases/verify_code_use_case.dart
+++ lib/features/auth/domain/usecases/verify_code_use_case.dart
@@ -1,16 +1,17 @@
 import 'package:data_sharing_organizing/core/status/status.dart';
 import 'package:data_sharing_organizing/core/usecase/usecase_with_parameter.dart';
+import 'package:data_sharing_organizing/core/utils/enums/verification_type_enum.dart';
 
 import '../../data/models/app_user/user.dart';
 import '../repositories/auth_repositories.dart';
 
-final class VerifyCodeUseCase extends UseCase<User, ({int id , int code})> {
+final class VerifyCodeUseCase extends UseCase<User, ({String code, int id, VerificationType verification})> {
   final AuthRepositories authRepositories;
 
   VerifyCodeUseCase(this.authRepositories);
 
   @override
-  Future<Status<User>> call(({int id , int code}) param) {
+  Future<Status<User>> call(({String code, int id, VerificationType verification}) param) {
     return authRepositories.verifyCode(param);
   }
 }
