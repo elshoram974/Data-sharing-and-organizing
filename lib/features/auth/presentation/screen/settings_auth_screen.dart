b80f158d3eef96bf9c9e1896bbbd73ed5919b80d
Mohammed Elshora <elshoram974@gmail.com>
2024-02-16 21:49:40 +0200

    fix cubit config dependency

--- lib/features/auth/presentation/screen/settings_auth_screen.dart
+++ lib/features/auth/presentation/screen/settings_auth_screen.dart
@@ -1,6 +1,9 @@
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../splash/presentation/cubit/config_cubit.dart';
 import '../widgets/auth_body.dart';
 import '../widgets/settings/dark_mode_list_tile.dart';
 import '../widgets/settings/locale_tile/language_list_tile.dart';
@@ -15,10 +18,11 @@ class AuthSettingsScreen extends StatelessWidget {
     final TextStyle? style = Theme.of(context).textTheme.titleLarge;
     final Color? color = style?.color;
     final double h = MediaQuery.sizeOf(context).height;
+    ProviderDependency.config = BlocProvider.of<ConfigCubit>(context);
 
     return Scaffold(
       body: AuthBody(
-        previousRouteNameFunction:(_)=> S.of(_).settings,
+        previousRouteNameFunction: (_) => S.of(_).settings,
         introHeader: S.of(context).settings,
         showSettingsButton: false,
         introBody: '',
