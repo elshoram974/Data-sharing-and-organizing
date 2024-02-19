82e2e310ee6e9d99d665cf3cb5cd60265b06a004
Mohammed Elshora <elshoram974@gmail.com>
2024-02-19 20:54:46 +0200

    fix app links

--- lib/core/utils/constants/app_links.dart
+++ lib/core/utils/constants/app_links.dart
@@ -1,12 +1,12 @@
 abstract final class AppLinks {
   const AppLinks();
 
-  static const String appLink = '';
+  static const String appLink = 'https://sharikna.mrecode.com';
 
-  static const String _baseLink = 'https://thiet.mrecode.com/api';
+  static const String _baseLink = 'https://thiet.mrecode.com/api/user';
 
   // * authorization links
-  static const String _auth = '$_baseLink/auth/users';
+  static const String _auth = '$_baseLink/auth';
   static const String login = '$_auth/login_user.php';
   static const String loginByProvider = '$_auth/login_provider.php';
   static const String signUp = '$_auth/sign_up_user.php';
