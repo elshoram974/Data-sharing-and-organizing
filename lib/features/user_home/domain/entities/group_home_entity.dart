994a1c4f19e748b7aff9b08506c56150c9704a3a
Mohammed Elshora <elshoram974@gmail.com>
2024-02-27 18:02:01 +0200

    make group home entity equitable and copy with

--- lib/features/user_home/domain/entities/group_home_entity.dart
+++ lib/features/user_home/domain/entities/group_home_entity.dart
@@ -1,15 +1,16 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-final class GroupHomeEntity {
+final class GroupHomeEntity extends Equatable {
   final String? imageLink;
   final String groupName;
   final InlineSpan? lastMessage;
   final int? unReadCounter;
   final bool isUnread;
   final DateTime lastMessageTime;
-  bool isSelected;
+  final bool isSelected;
 
-  GroupHomeEntity({
+  const GroupHomeEntity({
     this.imageLink,
     required this.groupName,
     this.lastMessage,
@@ -18,4 +19,35 @@ final class GroupHomeEntity {
     required this.lastMessageTime,
     this.isSelected = false,
   });
+
+  GroupHomeEntity copyWith({
+    String? imageLink,
+    String? groupName,
+    InlineSpan? lastMessage,
+    int? unReadCounter,
+    bool? isUnread,
+    DateTime? lastMessageTime,
+    bool? isSelected = false,
+  }) {
+    return GroupHomeEntity(
+      lastMessage: lastMessage ?? this.lastMessage,
+      unReadCounter: unReadCounter ?? this.unReadCounter,
+      isSelected: isSelected ?? this.isSelected,
+      isUnread: isUnread ?? this.isUnread,
+      imageLink: imageLink ?? this.imageLink,
+      groupName: groupName ?? this.groupName,
+      lastMessageTime: lastMessageTime ?? this.lastMessageTime,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        imageLink,
+        groupName,
+        lastMessage,
+        unReadCounter,
+        isUnread,
+        lastMessageTime,
+        isSelected
+      ];
 }
