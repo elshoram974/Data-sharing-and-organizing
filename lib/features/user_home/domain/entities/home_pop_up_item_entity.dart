763e71cd6e22703d63cf0f3ee9a37b1234268634
Mohammed Elshora <elshoram974@gmail.com>
2024-02-29 00:31:55 +0200

    HomePopUpItemEntity

---
+++ lib/features/user_home/domain/entities/home_pop_up_item_entity.dart
@@ -0,0 +1,55 @@
+import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/enums/home_selected_pop_up_enum.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../../presentation/cubit/user_home_cubit/user_home_cubit.dart';
+
+final class HomePopUpItemEntity extends Equatable {
+  final HomeSelectedPopUpItem value;
+  final String text;
+  final bool isVisible;
+
+  const HomePopUpItemEntity({
+    required this.text,
+    required this.value,
+    required this.isVisible,
+  });
+  @override
+  List<Object?> get props => [value, text];
+
+  static List<HomePopUpItemEntity> list(
+    BuildContext context,
+    UserHomeCubit cubit,
+  ) {
+    return [
+      HomePopUpItemEntity(
+        text: S.of(context).exitGroup,
+        value: HomeSelectedPopUpItem.exitGroup,
+        isVisible: true,
+      ),
+      HomePopUpItemEntity(
+        text: S.of(context).markAsUnRead,
+        value: HomeSelectedPopUpItem.markAsUnRead,
+        isVisible: true,
+      ),
+      HomePopUpItemEntity(
+        text: S.of(context).selectAll,
+        value: HomeSelectedPopUpItem.selectAll,
+        isVisible: !cubit.isAllSelected,
+      ),
+      HomePopUpItemEntity(
+        text: S.of(context).muteNotification,
+        value: HomeSelectedPopUpItem.muteNotification,
+        isVisible: cubit.selectedGroups.length == 1 &&
+            !cubit.selectedGroups.first.isMute,
+      ),
+      HomePopUpItemEntity(
+        text: S.of(context).unmuteNotification,
+        value: HomeSelectedPopUpItem.unmuteNotification,
+        isVisible: cubit.selectedGroups.length == 1 &&
+            cubit.selectedGroups.first.isMute,
+      ),
+    ];
+  }
+}
