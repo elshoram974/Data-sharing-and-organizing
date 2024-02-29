6fe7235c47cddd40c5ce15a2f1dd2f9afd991858
Mohammed Elshora <elshoram974@gmail.com>
2024-03-01 00:46:20 +0200

    removeSomeGroups in HomeRemoteDataSource

--- lib/features/user_home/data/datasources/home_datasources/home_remote_data_sources.dart
+++ lib/features/user_home/data/datasources/home_datasources/home_remote_data_sources.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:data_sharing_organizing/core/utils/constants/app_links.dart';
 import 'package:data_sharing_organizing/core/utils/services/api_services.dart';
 
@@ -6,19 +8,49 @@ import '../../../domain/entities/group_home_entity.dart';
 
 abstract class HomeRemoteDataSource {
   const HomeRemoteDataSource();
-  Future<List<GroupHomeEntity>> getGroups(AuthUserEntity user, int page);
+  Future<List<GroupHomeEntity>> getGroups(
+    AuthUserEntity user,
+    int page,
+    int pageSize,
+  );
+  Future<List<GroupHomeEntity>> removeSomeGroups(
+    AuthUserEntity user,
+    List<GroupHomeEntity> removedGroups,
+  );
 }
 
 class HomeRemoteDataSourceImp extends HomeRemoteDataSource {
   final APIServices service;
 
-  HomeRemoteDataSourceImp(this.service);
+  const HomeRemoteDataSourceImp(this.service);
 
   @override
-  Future<List<GroupHomeEntity>> getGroups(AuthUserEntity user, int page) async {
+  Future<List<GroupHomeEntity>> getGroups(
+    AuthUserEntity user,
+    int page,
+    int pageSize,
+  ) async {
     Map<String, dynamic> response = await service.post(
       AppLinks.getGroups,
       {'userId': user.id, 'pageNumber': page, 'groupsPerPage': 10},
+    );
+    print(response);
+    return [];
+  }
+
+  @override
+  Future<List<GroupHomeEntity>> removeSomeGroups(
+    AuthUserEntity user,
+    List<GroupHomeEntity> removedGroups,
+  ) async {
+    final List<int> groupsIds = [];
+    for (final e in removedGroups) {
+      groupsIds.add(e.id);
+    }
+    // TODO: implement removeSomeGroups
+    Map<String, dynamic> response = await service.post(
+      'AppLinks.getGroups',
+      {'userId': user.id, 'removedGroupsIds': json.encode(groupsIds)},
     );
     print(response);
     return [];
