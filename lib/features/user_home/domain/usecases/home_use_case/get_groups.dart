692f8300db741912f6172e178edbf9587e897720
Mohammed Elshora <elshoram974@gmail.com>
2024-02-29 22:53:15 +0200

    GetGroupsUseCase

---
+++ lib/features/user_home/domain/usecases/home_use_case/get_groups.dart
@@ -0,0 +1,17 @@
+import 'package:data_sharing_organizing/core/status/status.dart';
+import 'package:data_sharing_organizing/core/usecase/usecase_with_parameter.dart';
+
+import '../../../../auth/domain/entities/auth_user_entity.dart';
+import '../../entities/group_home_entity.dart';
+import '../../repositories/home_repositories.dart';
+
+final class GetGroupsUseCase extends UseCase<List<GroupHomeEntity>, ({AuthUserEntity user, int page})> {
+  final HomeRepositories homeRepositories;
+
+  GetGroupsUseCase(this.homeRepositories);
+
+  @override
+  Future<Status<List<GroupHomeEntity>>> call(({AuthUserEntity user, int page}) param) {
+    return homeRepositories.getGroups(param);
+  }
+}
