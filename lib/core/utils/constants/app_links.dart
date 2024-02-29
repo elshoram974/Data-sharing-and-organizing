a2ebcdda8f0c7958b4ab263a528695dec9035fba
Mohammed Elshora <elshoram974@gmail.com>
2024-02-29 23:00:02 +0200

    add links for home and profile

--- lib/core/utils/constants/app_links.dart
+++ lib/core/utils/constants/app_links.dart
@@ -13,4 +13,11 @@ abstract final class AppLinks {
   static const String sendVerifyCode = '$_auth/send_verify_code.php';
   static const String checkVerifyCode = '$_auth/check_verify_code.php';
   static const String newPassword = '$_auth/new_password.php';
+
+  // * user home
+  static const String _home = '$_baseLink/home';
+  static const String getGroups = '$_home/getGroups.php';
+
+  // * user profile
+  // static const String _profile = '$_baseLink/profile';
 }
