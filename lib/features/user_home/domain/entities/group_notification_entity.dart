174c27951c34b13ef052b70fb19286a4cf969056
Mohammed Elshora <elshoram974@gmail.com>
2024-02-29 13:26:14 +0200

    make equatable in group notification entity

--- lib/features/user_home/domain/entities/group_notification_entity.dart
+++ lib/features/user_home/domain/entities/group_notification_entity.dart
@@ -1,14 +1,15 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-final class GroupNotificationEntity {
+final class GroupNotificationEntity extends Equatable {
   final String? imageLink;
   final String groupName;
   final InlineSpan? lastMessage;
   final bool isUnread;
   final DateTime lastMessageTime;
-  bool isExpanded;
+  final bool isExpanded;
 
-  GroupNotificationEntity({
+  const GroupNotificationEntity({
     this.imageLink,
     required this.groupName,
     this.lastMessage,
@@ -16,4 +17,32 @@ final class GroupNotificationEntity {
     required this.lastMessageTime,
     this.isExpanded = false,
   });
+
+  GroupNotificationEntity copyWith({
+    String? imageLink,
+    String? groupName,
+    InlineSpan? lastMessage,
+    bool? isUnread,
+    DateTime? lastMessageTime,
+    bool? isExpanded,
+  }) {
+    return GroupNotificationEntity(
+      lastMessage: lastMessage ?? this.lastMessage,
+      isExpanded: isExpanded ?? this.isExpanded,
+      isUnread: isUnread ?? this.isUnread,
+      imageLink: imageLink ?? this.imageLink,
+      groupName: groupName ?? this.groupName,
+      lastMessageTime: lastMessageTime ?? this.lastMessageTime,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        imageLink,
+        groupName,
+        lastMessage,
+        isUnread,
+        lastMessageTime,
+        isExpanded,
+      ];
 }
