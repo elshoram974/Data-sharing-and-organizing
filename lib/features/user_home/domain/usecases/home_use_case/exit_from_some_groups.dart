63ba0a3205ed4fba6b7cb5e4ea63b5e69dbf3d4d
Mohammed Elshora <elshoram974@gmail.com>
2024-03-07 08:41:22 +0200

    ExitFromSomeGroups usecase

---
+++ lib/features/user_home/domain/usecases/home_use_case/exit_from_some_groups.dart
@@ -0,0 +1,17 @@
+import 'package:data_sharing_organizing/core/status/status.dart';
+import 'package:data_sharing_organizing/core/usecase/usecase_with_parameter.dart';
+
+import '../../../../auth/domain/entities/auth_user_entity.dart';
+import '../../entities/group_home_entity.dart';
+import '../../repositories/home_repositories.dart';
+
+final class ExitFromSomeGroups extends UseCase<bool, ({List<GroupHomeEntity> removedGroups, AuthUserEntity user})> {
+  final HomeRepositories homeRepositories;
+
+  ExitFromSomeGroups(this.homeRepositories);
+
+  @override
+  Future<Status<bool>> call(({List<GroupHomeEntity> removedGroups, AuthUserEntity user}) param) {
+    return homeRepositories.exitFromSomeGroups(param);
+  }
+}
