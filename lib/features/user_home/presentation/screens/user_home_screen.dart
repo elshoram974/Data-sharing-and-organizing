e56504876ffd5f93d37e443d742d92333b204dd6
Mohammed Elshora <elshoram974@gmail.com>
2024-03-07 11:12:08 +0200

    make RefreshIndicator in home

--- lib/features/user_home/presentation/screens/user_home_screen.dart
+++ lib/features/user_home/presentation/screens/user_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:data_sharing_organizing/core/shared/empty_page_text.dart';
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
 import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,22 +18,31 @@ class UserHomeScreen extends StatelessWidget {
     final UserHomeCubit c = ProviderDependency.userHome;
     return BlocBuilder<UserHomeCubit, UserHomeState>(
       builder: (context, state) {
-        return MainBodyWidget(
-          children: [
-            if (c.currentGroups.isEmpty)
-              EmptyPageText(S.of(context).youCanMakeNewGroups),
-            ...List.generate(
-              c.currentGroups.length,
-              (index) {
-                final GroupHomeEntity group = c.currentGroups[index];
-                return HomeGroupTile(
-                  onTap: () => c.onTapGroup(group),
-                  onLongPress: () => c.onLongTapGroup(group),
-                  groupHomeEntity: group,
-                );
-              },
-            ),
-          ],
+        return RefreshIndicator(
+          onRefresh: c.getGroups,
+          child: MainBodyWidget(
+            children: [
+              if (state is GetGroupsLoadingState)
+                Container(
+                  padding: const EdgeInsets.all(AppConst.defaultPadding),
+                  alignment: Alignment.center,
+                  child: const CircularProgressIndicator(),
+                ),
+              if (c.currentGroups.isEmpty)
+                EmptyPageText(S.of(context).youCanMakeNewGroups),
+              ...List.generate(
+                c.currentGroups.length,
+                (index) {
+                  final GroupHomeEntity group = c.currentGroups[index];
+                  return HomeGroupTile(
+                    onTap: () => c.onTapGroup(group),
+                    onLongPress: () => c.onLongTapGroup(group),
+                    groupHomeEntity: group,
+                  );
+                },
+              ),
+            ],
+          ),
         );
       },
     );
