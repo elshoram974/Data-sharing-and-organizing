9c2fd2e40d582a130834917d334cd27ebf86c9d8
Mohammed Elshora <elshoram974@gmail.com>
2024-03-10 18:54:14 +0200

    EditProfileLocalDataSource

---
+++ lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_local_data_sources.dart
@@ -0,0 +1,7 @@
+abstract class EditProfileLocalDataSource {
+  const EditProfileLocalDataSource();
+}
+
+class EditProfileLocalDataSourceImp extends EditProfileLocalDataSource {
+  EditProfileLocalDataSourceImp();
+}
