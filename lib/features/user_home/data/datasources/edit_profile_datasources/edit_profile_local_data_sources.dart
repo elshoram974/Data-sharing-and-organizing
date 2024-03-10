d6420caf6fd548dfc0e03019dd6554c2eeb0e639
Mohammed Elshora <elshoram974@gmail.com>
2024-03-10 19:47:44 +0200

    changePassword in EditProfileLocalDataSource

--- lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_local_data_sources.dart
+++ lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_local_data_sources.dart
@@ -1,7 +1,24 @@
+import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../../../../auth/domain/entities/auth_user_entity.dart';
+
 abstract class EditProfileLocalDataSource {
   const EditProfileLocalDataSource();
+
+  Future<AuthUserEntity> changePassword(String newPass);
 }
 
 class EditProfileLocalDataSourceImp extends EditProfileLocalDataSource {
   EditProfileLocalDataSourceImp();
+
+  late final Box<AuthUserEntity> _userBox = Hive.box<AuthUserEntity>(AppStrings.userBox);
+
+  @override
+  Future<AuthUserEntity> changePassword(String newPass) async {
+    final AuthUserEntity savedUser = _userBox.values.last.copyWith(password: newPass);
+    _userBox.clear();
+    await _userBox.add(savedUser);
+    return savedUser;
+  }
 }
