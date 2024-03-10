49122e33de9166c0fd76a91a1e419a32d78122c4
Mohammed Elshora <elshoram974@gmail.com>
2024-03-10 19:39:29 +0200

    changePass in EditProfileRemoteDataSource

--- lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_remote_data_sources.dart
+++ lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_remote_data_sources.dart
@@ -1,10 +1,12 @@
 import 'package:data_sharing_organizing/core/utils/services/api_services.dart';
 
+import '../../../../auth/data/models/app_user/app_user.dart';
 import '../../../../auth/domain/entities/auth_user_entity.dart';
 
 abstract class EditProfileRemoteDataSource {
   const EditProfileRemoteDataSource();
   Future<bool> deleteAccount(AuthUserEntity user);
+  Future<AuthUserEntity> changePass(int userId, String pass, String newPass);
 }
 
 class EditProfileRemoteDataSourceImp extends EditProfileRemoteDataSource {
@@ -15,10 +17,19 @@ class EditProfileRemoteDataSourceImp extends EditProfileRemoteDataSource {
   @override
   Future<bool> deleteAccount(AuthUserEntity user) async {
     Map<String, dynamic> response = await service.post(
-      'AppLinks.delete', // TODO: change delete link
+      'AppLinks.delete', // TODO: delete link
       {'userId': '${user.id}'},
     );
     print(response);
     return response['status'] == 'success';
+  }
+
+  @override
+  Future<AuthUserEntity> changePass(int userId, String pass, String newPass) async {
+    Map<String, dynamic> response = await service.post(
+      'AppLinks.change pass', // TODO: change pass link
+      {'userId': '$userId', 'currentPass': pass, 'newPass': newPass},
+    );
+    return AppUser.fromMap(response).user!;
   }
 }
