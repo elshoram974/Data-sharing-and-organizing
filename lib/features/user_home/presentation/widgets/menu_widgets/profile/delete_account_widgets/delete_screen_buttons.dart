cbf258554f2d1bafb9b551a0ce3bb02f580cc307
Mohammed Elshora <elshoram974@gmail.com>
2024-03-10 19:31:25 +0200

    end delete account screen with the logic

--- lib/features/user_home/presentation/widgets/menu_widgets/profile/delete_account_widgets/delete_screen_buttons.dart
+++ lib/features/user_home/presentation/widgets/menu_widgets/profile/delete_account_widgets/delete_screen_buttons.dart
@@ -1,9 +1,18 @@
 import 'package:data_sharing_organizing/core/shared/filled_button.dart';
+import 'package:data_sharing_organizing/core/status/errors/failure.dart';
+import 'package:data_sharing_organizing/core/status/status.dart';
+import 'package:data_sharing_organizing/core/status/success/success.dart';
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/config/routes/routes.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/locator.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../../../domain/repositories/edit_profile_repositories.dart';
 
 class DeleteScreenButtons extends StatelessWidget {
   const DeleteScreenButtons({super.key});
@@ -23,9 +32,29 @@ class DeleteScreenButtons extends StatelessWidget {
         MyFilledButton(
           text: S.of(context).delete,
           minimumSize: const Size(126, 48),
-          onPressed: () {},
+          onPressed: () => onPressDelete(context),
         ),
       ],
     );
+  }
+
+  Future<void> onPressDelete(BuildContext context) async {
+    EasyLoading.show(dismissOnTap: false);
+    final Status<bool> status = await sl.get<EditProfileRepositories>().deleteAccount(ProviderDependency.userMain.user);
+    EasyLoading.dismiss();
+
+    if (status is Success<bool>) {
+      if (status.data) {
+        ProviderDependency.userMain.navIndex = 0 ;
+        if (context.mounted) context.go(AppRoute.login);
+      } else {
+        EasyLoading.showError('Refused', duration: const Duration(seconds: 5));
+      }
+    } else {
+      EasyLoading.showError(
+        (status as Failure<bool>).error,
+        duration: const Duration(seconds: 5),
+      );
+    }
   }
 }
