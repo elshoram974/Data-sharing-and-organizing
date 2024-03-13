85f6f8fa42af84eb13da41be808758f6e93a93f0
Mohammed Elshora <elshoram974@gmail.com>
2024-03-13 13:05:56 +0200

    delete Image and change it

--- lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_local_data_sources.dart
+++ lib/features/user_home/data/datasources/edit_profile_datasources/edit_profile_local_data_sources.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -8,6 +9,8 @@ abstract class EditProfileLocalDataSource {
 
   Future<AuthUserEntity> changePassword(String newPass);
   Future<AuthUserEntity> changeName(String fName, String lName);
+  Future<AuthUserEntity> changeImage(String imageLink , String newLink);
+  Future<AuthUserEntity> deleteImage(String imageLink);
 }
 
 class EditProfileLocalDataSourceImp extends EditProfileLocalDataSource {
@@ -31,5 +34,31 @@ class EditProfileLocalDataSourceImp extends EditProfileLocalDataSource {
     await _userBox.clear();
     await _userBox.add(savedUser);
     return savedUser;
+  }
+  
+  @override
+  Future<AuthUserEntity> changeImage(String imageLink , String newLink) async{
+    final AuthUserEntity user = _userBox.values.last.copyWith(image: newLink);
+    await Future.wait([_userBox.clear(),CachedNetworkImage.evictFromCache(imageLink)]);
+   
+    await _userBox.add(user);
+
+    return user;
+  }
+  @override
+  Future<AuthUserEntity> deleteImage(String imageLink) async{
+    AuthUserEntity user = _userBox.values.last;
+    await Future.wait([_userBox.clear(),CachedNetworkImage.evictFromCache(imageLink)]);
+    user = AuthUserEntity(
+          image: null,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          userType: user.userType,
+        );
+    await _userBox.add(user);
+
+    return user;
   }
 }
