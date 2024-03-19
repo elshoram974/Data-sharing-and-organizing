57c6e7dffb5a325f499d8f9540344fbe83bd6518
Mohammed Elshora <elshoram974@gmail.com>
2024-03-19 11:25:42 +0200

    Update user-related links in AppLinks constants
    Update user-related links in AppLinks constants to use the new `_userLink`
    variable for consistency and clarity. This change ensures that all user-related
    links are derived from the same base URL.

--- lib/core/utils/constants/app_links.dart
+++ lib/core/utils/constants/app_links.dart
@@ -3,10 +3,11 @@ abstract final class AppLinks {
 
   static const String appLink = 'https://sharikna.mrecode.com';
 
-  static const String _baseLink = 'https://thiet.mrecode.com/api/user';
+  static const String _baseLink = 'https://thiet.mrecode.com/api';
+  static const String _userLink = '$_baseLink/user';
 
   // * authorization links
-  static const String _auth = '$_baseLink/auth';
+  static const String _auth = '$_userLink/auth';
   static const String login = '$_auth/login_user.php';
   static const String loginByProvider = '$_auth/login_provider.php';
   static const String signUp = '$_auth/sign_up_user.php';
@@ -15,12 +16,12 @@ abstract final class AppLinks {
   static const String newPassword = '$_auth/new_password.php';
 
   // * user home
-  static const String _home = '$_baseLink/home';
+  static const String _home = '$_userLink/home';
   static const String userHome = '$_home/home.php';
   static const String removeGroups = '$_home/removeGroups.php';
 
   // * user profile
-  static const String _changes = '$_baseLink/changes';
+  static const String _changes = '$_userLink/changes';
   static const String changeName = '$_changes/changename.php';
   static const String changePassword = '$_changes/changepassword.php';
   static const String deleteUser = '$_changes/deleteuser.php';
