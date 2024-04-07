8dfd756c49fe31f82d2113507bfaee5c60b60ef2
Mohammed Elshora <elshoram974@gmail.com>
2024-04-07 15:35:14 +0200

    Update group screen to display chat messages in a chat page
    Refactor the UserGroupScreen to display chat messages in a separate ChatPage
    component. Add functionality to send messages, attach files, and display
    avatars and user names in the chat.

--- lib/features/chat/presentation/screens/group_screen.dart
+++ lib/features/chat/presentation/screens/group_screen.dart
@@ -1,6 +1,25 @@
+import 'package:bubble/bubble.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
 import 'package:data_sharing_organizing/core/utils/enums/user_role/user_type_enum.dart';
+import 'package:data_sharing_organizing/core/utils/functions/convert_date_to_string.dart';
 import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
+import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../../auth/domain/entities/auth_user_entity.dart';
 import '../../../user_home/domain/entities/group_home_entity.dart';
@@ -14,31 +33,280 @@ class UserGroupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          GroupAppBar(group: group),
-          Expanded(
-            child: CustomScrollView(
-              slivers: [
-                ...List.generate(
-                  20,
-                  (index) => MessageWidget(
-                    messageCreator: index % 2 == 0
-                        ? const AuthUserEntity(
-                            id: 20,
-                            name: 'name',
-                            email: 'email',
-                            password: 'password',
-                            userType: UserType.personal)
-                        : ProviderDependency.userMain.user,
-                  ),
+    return ChatPage();
+    // return Scaffold(
+    //   body: Column(
+    //     children: [
+    //       GroupAppBar(group: group),
+    //       Expanded(
+    //         child: CustomScrollView(
+    //           slivers: [
+    //             ...List.generate(
+    //               20,
+    //               (index) => MessageWidget(
+    //                 messageCreator: index % 2 == 0
+    //                     ? const AuthUserEntity(
+    //                         id: 20,
+    //                         name: 'name',
+    //                         email: 'email',
+    //                         password: 'password',
+    //                         userType: UserType.personal)
+    //                     : ProviderDependency.userMain.user,
+    //               ),
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  List<types.Message> _messages = [];
+  final _user = const types.User(
+    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
                 ),
-              ],
-            ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleFileSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('File'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
+    );
+
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: const Uuid().v4(),
+        mimeType: lookupMimeType(result.files.single.path!),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleImageSelection() async {
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: ImageSource.gallery,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: const Uuid().v4(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      var localPath = message.uri;
+
+      if (message.uri.startsWith('http')) {
+        try {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: true,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+
+          final client = http.Client();
+          final request = await client.get(Uri.parse(message.uri));
+          final bytes = request.bodyBytes;
+          final documentsDir = (await getApplicationDocumentsDirectory()).path;
+          localPath = '$documentsDir/${message.name}';
+
+          if (!File(localPath).existsSync()) {
+            final file = File(localPath);
+            await file.writeAsBytes(bytes);
+          }
+        } finally {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: null,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+        }
+      }
+
+      await OpenFilex.open(localPath);
+    }
+  }
+
+  void _handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void _loadMessages() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    setState(() {
+      _messages = messages;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Chat(
+          onAvatarTap: (p0) {},
+          onMessageLongPress: (context, p1) {},
+          messages: _messages,
+          onAttachmentPressed: _handleAttachmentPressed,
+          onMessageTap: _handleMessageTap,
+          onPreviewDataFetched: _handlePreviewDataFetched,
+          onSendPressed: _handleSendPressed,
+          showUserAvatars: true,
+          showUserNames: true,
+          bubbleBuilder: (
+            Widget child, {
+            required message,
+            required nextMessageInGroup,
+          }) =>
+              Bubble(
+            padding: const BubbleEdges.all(0),
+            radius: const Radius.circular(5),
+            color: _user.id != message.author.id
+                ? const Color(0xFFC8C8C8)
+                : AppColor.primary,
+            margin: nextMessageInGroup
+                ? const BubbleEdges.symmetric(horizontal: 6)
+                : null,
+            nip: nextMessageInGroup
+                ? BubbleNip.no
+                : _user.id != message.author.id
+                    ? BubbleNip.leftBottom
+                    : BubbleNip.rightBottom,
+            child: child,
+          ),
+          user: _user,
+          theme: DefaultChatTheme(
+            primaryColor: AppColor.primary,
+            backgroundColor: AppColor.background(context),
+            inputBorderRadius: BorderRadius.zero,
+            secondaryColor: const Color(0xFFC8C8C8),
+            messageBorderRadius: 5,
+            messageInsetsVertical: 8,
+            messageInsetsHorizontal: 8,
+            sentMessageBodyTextStyle: AppStyle.styleBoldInika13,
+            inputTextStyle: AppStyle.styleBoldInika13,
+            attachmentButtonIcon: const Icon(
+              Icons.attach_file_outlined,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
 }
