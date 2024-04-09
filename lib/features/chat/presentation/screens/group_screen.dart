f4f97fc8535a57b3e9ddc50d990a0b46063b3563
Mohammed Elshora <elshoram974@gmail.com>
2024-04-09 17:50:38 +0200

    Add GroupChatScreen and update UserGroupScreen
    - Added GroupChatScreen for group chat functionality
    - Updated UserGroupScreen to include floating screen selection button and
      functionality for switching between screens.

--- lib/features/chat/presentation/screens/group_screen.dart
+++ lib/features/chat/presentation/screens/group_screen.dart
@@ -1,297 +1,62 @@
-import 'package:bubble/bubble.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
-import 'package:data_sharing_organizing/core/utils/functions/convert_date_to_string.dart';
-import 'package:data_sharing_organizing/core/utils/functions/detect_text_direction.dart';
 import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_chat_ui/src/models/date_header.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
-import 'package:intl/intl.dart' as intl;
-import 'package:mime/mime.dart';
-import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:uuid/uuid.dart';
-
 import '../../../user_home/domain/entities/group_home_entity.dart';
 import '../widgets/group_app_bar.dart';
+import 'group_chat_screen.dart';
 
-class UserGroupScreen extends StatelessWidget {
+class UserGroupScreen extends StatefulWidget {
   const UserGroupScreen({super.key, required this.group});
 
   final GroupHomeEntity group;
 
   @override
-  Widget build(BuildContext context) {
-    return ChatScreen(group: group);
-  }
+  State<UserGroupScreen> createState() => _UserGroupScreenState();
 }
 
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key, required this.group});
-  final GroupHomeEntity group;
+class _UserGroupScreenState extends State<UserGroupScreen> {
+  bool isOpened = false;
 
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
+  int currentScreen = 0;
 
-class _ChatScreenState extends State<ChatScreen> {
-  List<types.Message> _messages = [
-    types.TextMessage(
-      createdAt: DateTime(2024, 4, 7, 16, 44).millisecondsSinceEpoch,
-      text: 'Hi there!\ni\'m Mohammed El Shora',
-      author: const types.User(
-        firstName: "Mohammed",
-        lastName: "El Shora",
-        role: types.Role.admin,
-        id: 'Mohammed El Shora',
-      ),
-      // duration: Duration(seconds: 5),
-      id: "aaa",
-    )
+  final List<Widget> screens = [
+    const Center(child: Text('Bot screen')),
+    const GroupChatScreen(),
   ];
-  final _user = const types.User(
-    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
-  );
+
+  double _dragPositionX = 0.0;
+  double _top = 0.0;
 
   @override
-  void initState() {
-    super.initState();
-    _loadMessages();
-  }
-
-  void _addMessage(types.Message message) {
-    setState(() {
-      _messages.insert(0, message);
-    });
-  }
-
-  void _handleAttachmentPressed() {
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) => SafeArea(
-        child: SizedBox(
-          height: 144,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _handleImageSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('Photo'),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _handleFileSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('File'),
-                ),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('Cancel'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _handleFileSelection() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.any,
-    );
-
-    if (result != null && result.files.single.path != null) {
-      final message = types.FileMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-        mimeType: lookupMimeType(result.files.single.path!),
-        name: result.files.single.name,
-        size: result.files.single.size,
-        uri: result.files.single.path!,
-      );
-
-      _addMessage(message);
-    }
-  }
-
-  void _handleImageSelection() async {
-    final result = await ImagePicker().pickImage(
-      imageQuality: 70,
-      maxWidth: 1440,
-      source: ImageSource.gallery,
-    );
-
-    if (result != null) {
-      final bytes = await result.readAsBytes();
-      final image = await decodeImageFromList(bytes);
-
-      final message = types.ImageMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        height: image.height.toDouble(),
-        id: const Uuid().v4(),
-        name: result.name,
-        size: bytes.length,
-        uri: result.path,
-        width: image.width.toDouble(),
-      );
-
-      _addMessage(message);
-    }
-  }
-
-  void _handleMessageTap(BuildContext _, types.Message message) async {
-    if (message is types.FileMessage) {
-      var localPath = message.uri;
-
-      if (message.uri.startsWith('http')) {
-        try {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: true,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-
-          final client = http.Client();
-          final request = await client.get(Uri.parse(message.uri));
-          final bytes = request.bodyBytes;
-          final documentsDir = (await getApplicationDocumentsDirectory()).path;
-          localPath = '$documentsDir/${message.name}';
-
-          if (!File(localPath).existsSync()) {
-            final file = File(localPath);
-            await file.writeAsBytes(bytes);
-          }
-        } finally {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: null,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-        }
-      }
-
-      await OpenFilex.open(localPath);
-    }
-  }
-
-  void _handlePreviewDataFetched(
-    types.TextMessage message,
-    types.PreviewData previewData,
-  ) {
-    final index = _messages.indexWhere((element) => element.id == message.id);
-    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
-      previewData: previewData,
-    );
-
-    setState(() {
-      _messages[index] = updatedMessage;
-    });
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: const Uuid().v4(),
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-  }
-
-  void _loadMessages() async {
-    final response = await rootBundle.loadString('assets/messages.json');
-    final messages = (jsonDecode(response) as List)
-        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    setState(() {
-      _messages = messages;
-    });
-  }
-
-  Directionality customBubble(
-    Widget child, {
-    required types.Message message,
-    required bool nextMessageInGroup,
-  }) {
-    final bool isTheUser = _user.id == message.author.id;
-    final bool isTextMessage = message is types.TextMessage;
-    const double border = 5;
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Column(
-        crossAxisAlignment:
-            isTheUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: GroupAppBar(group: widget.group),
+      body: Stack(
         children: [
-          Bubble(
-            padding: isTextMessage
-                ? const BubbleEdges.all(0)
-                : const BubbleEdges.all(border),
-            radius: const Radius.circular(0.5 * AppConst.borderRadius),
-            color: isTheUser
-                ? AppColor.primary
-                : AppColor.grayLightDark(context),
-            margin: nextMessageInGroup
-                ? const BubbleEdges.symmetric(horizontal: 6)
-                : null,
-            nip: nextMessageInGroup
-                ? BubbleNip.no
-                : isTheUser
-                    ? BubbleNip.rightBottom
-                    : BubbleNip.leftBottom,
-            child: Directionality(
-              textDirection: detectRtlDirectionality(
-                isTextMessage ? message.text : 'A',
+          screens[currentScreen],
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            left: isOpened ? 0 : -118,
+            top: _top,
+            child: GestureDetector(
+              onTap: () => setState(() => isOpened = !isOpened),
+              onHorizontalDragStart: (d) => _dragPositionX = d.localPosition.dx,
+              onHorizontalDragUpdate: (d) => onHorizontalDrag(_dragPositionX, d),
+              onPanUpdate: (details) {
+                setState(() {
+                  _top += details.delta.dy;
+                  final height = MediaQuery.of(context).size.height - 180;
+                  if (_top < 0) _top = 0;
+                  if (_top > height) _top = height;
+                });
+              },
+              child: FloatingSelectingScreenButton(
+                isOpened: isOpened,
+                currentScreen: currentScreen,
+                onChooseScreen: onChooseScreen,
               ),
-              child: child,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 6,
-              vertical: 2,
-            ),
-            child: Text(
-              intl.DateFormat.jm().format(
-                DateTime.fromMillisecondsSinceEpoch(message.createdAt ?? 0),
-              ),
-              style: const TextStyle(fontSize: 8, color: AppColor.gray),
             ),
           ),
         ],
@@ -299,100 +64,108 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  final TextStyle linkDescription =
-      const TextStyle(color: Colors.black, fontSize: 11);
-  final TextStyle linkTitle =
-      const TextStyle(color: Colors.black, fontWeight: FontWeight.w600);
-  final TextStyle link = TextStyle(
-    color: Colors.blue.shade300,
-    decoration: TextDecoration.underline,
-  );
+  void onHorizontalDrag(double dragPositionX, DragUpdateDetails d) {
+    final double delta = dragPositionX - d.localPosition.dx;
+    if (delta < 0) {
+      // drag to right
+      if (isOpened) return;
+      isOpened = true;
+    } else {
+      // drag to left
+      if (!isOpened) return;
+      isOpened = false;
+    }
+    setState(() {});
+    return;
+  }
+
+  void onChooseScreen(int chosenScreen) {
+    currentScreen = chosenScreen;
+    isOpened = false;
+    setState(() {});
+  }
+}
+
+class FloatingSelectingScreenButton extends StatelessWidget {
+  const FloatingSelectingScreenButton({
+    super.key,
+    required this.isOpened,
+    required this.currentScreen,
+    required this.onChooseScreen,
+  });
+
+  final bool isOpened;
+  final int currentScreen;
+  final void Function(int chosenScreen) onChooseScreen;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: GroupAppBar(group: widget.group),
-      body: Chat(
-        onAvatarTap: (p0) {},
-        onMessageLongPress: (context, p1) {
-          print(p1);
-        },
-        messages: _messages,
-        onAttachmentPressed: _handleAttachmentPressed,
-        onMessageTap: _handleMessageTap,
-        onPreviewDataFetched: _handlePreviewDataFetched,
-        onSendPressed: _handleSendPressed,
-        showUserAvatars: true,
-        showUserNames: true,
-        bubbleBuilder: customBubble,
-        dateHeaderBuilder: (_) => dateHeader(context, _),
-        inputOptions: InputOptions(
-          onTextChanged: (val) => setState(() {}),
-        ),
-        user: _user,
-        theme: DefaultChatTheme(
-          inputMargin: const EdgeInsets.only(
-            left: AppConst.defaultPadding,
-            right: AppConst.defaultPadding,
-            bottom: 0.5 * AppConst.defaultPadding,
-          ),
-          primaryColor: AppColor.primary,
-          backgroundColor: AppColor.background(context),
-          inputBorderRadius: BorderRadius.circular(15),
-          secondaryColor: AppColor.grayLightDark(context),
-          messageBorderRadius: 0.5 * AppConst.borderRadius,
-          messageInsetsVertical: 8,
-          messageInsetsHorizontal: 8,
-          receivedMessageBodyLinkTextStyle: link,
-          receivedMessageLinkTitleTextStyle: linkTitle,
-          receivedMessageLinkDescriptionTextStyle: linkDescription,
-          sentMessageBodyLinkTextStyle: link,
-          sentMessageLinkTitleTextStyle: linkTitle,
-          sentMessageLinkDescriptionTextStyle: linkDescription,
-          sentMessageBodyTextStyle: AppStyle.styleBoldInika13,
-          receivedMessageBodyTextStyle: AppStyle.styleBoldInika13,
-          inputTextStyle: AppStyle.styleBoldInika13,
-          inputElevation: 1,
-          inputPadding: EdgeInsets.zero,
-          inputBackgroundColor: AppColor.grayLightDark(context),
-          attachmentButtonIcon: const MyAttachmentButtonIcon(),
+    return Container(
+      height: 92,
+      width: 134,
+      decoration: const BoxDecoration(
+        color: AppColor.active,
+        borderRadius: BorderRadius.horizontal(
+          right: Radius.circular(AppConst.borderRadius),
         ),
       ),
-    );
-  }
-
-  Bubble dateHeader(BuildContext context, DateHeader dateHeader) {
-    return Bubble(
-      margin: const BubbleEdges.only(
-        top: AppConst.defaultPadding,
-        bottom: 0.5 * AppConst.defaultPadding,
-      ),
-      alignment: Alignment.center,
-      nip: BubbleNip.no,
-      color: AppColor.grayLightDark(context),
-      child: Text(
-        DateToString.call(dateHeader.dateTime, true),
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontSize: 11.0,
-          color: Colors.black38,
-          fontWeight: FontWeight.bold,
+      child: AnimatedOpacity(
+        duration: const Duration(milliseconds: 300),
+        opacity: isOpened ? 1 : 0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SelectBotButton(
+              text: 'BOT',
+              onPressed: isOpened ? () => onChooseScreen(0) : null,
+              backgroundColor: currentScreen == 0
+                  ? AppColor.primary
+                  : AppColor.grayLightDark(context),
+            ),
+            SelectBotButton(
+              text: 'Chat',
+              onPressed: isOpened ? () => onChooseScreen(1) : null,
+              backgroundColor: currentScreen == 1
+                  ? AppColor.primary
+                  : AppColor.grayLightDark(context),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class MyAttachmentButtonIcon extends StatelessWidget {
-  const MyAttachmentButtonIcon({super.key});
+class SelectBotButton extends StatelessWidget {
+  const SelectBotButton({
+    super.key,
+    this.onPressed,
+    this.backgroundColor,
+    required this.text,
+  });
+
+  final void Function()? onPressed;
+  final Color? backgroundColor;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: 45,
-      child: const Icon(
-        Icons.attach_file_outlined,
-        color: Colors.white,
+    return Flexible(
+      child: FilledButton(
+        onPressed: onPressed,
+        style: FilledButton.styleFrom(
+          minimumSize: const Size(double.maxFinite, 27),
+          backgroundColor: backgroundColor,
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppConst.borderRadius),
+          ),
+        ),
+        child: Text(
+          text,
+          style: AppStyle.styleBoldInika13.copyWith(fontSize: 14),
+        ),
       ),
     );
   }
