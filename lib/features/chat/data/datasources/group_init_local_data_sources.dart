dd101ba9c2c86b1bb23352b8746fd1965023331e
Mohammed Elshora <elshoram974@gmail.com>
2024-04-11 12:57:01 +0200

    Add GroupInitLocalDataSource implementation
    Implement GroupInitLocalDataSource interface with methods to get and save
    floating place data. This class provides functionality for interacting with
    local data related to group initialization in the chat feature.

---
+++ lib/features/chat/data/datasources/group_init_local_data_sources.dart
@@ -0,0 +1,22 @@
+import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
+import 'package:data_sharing_organizing/core/utils/services/init_local.dart';
+
+abstract class GroupInitLocalDataSource {
+  const GroupInitLocalDataSource();
+  double? getFloatingPlace();
+  Future<void> saveFloatingPlace(double newTop);
+}
+
+class GroupInitLocalDataSourceImp extends GroupInitLocalDataSource {
+  GroupInitLocalDataSourceImp();
+
+  @override
+  double? getFloatingPlace() {
+    return double.tryParse('${config.get(AppStrings.floatingButtonPlace)}');
+  }
+
+  @override
+  Future<void> saveFloatingPlace(double newTop) {
+    return config.put(AppStrings.floatingButtonPlace, '$newTop');
+  }
+}
