71a917206f9d34a0de6735bd09e867530a0ae7ae
Mohammed Elshora <elshoram974@gmail.com>
2024-04-11 13:07:18 +0200

    Add initial implementation of GroupInitRepositories
    Implemented GroupInitRepositories interface with methods to get and save
    button placement in the local data source. This initial implementation
    provides functionality to interact with the local data source for group
    initialization in the chat feature.

---
+++ lib/features/chat/data/repositories/init_group_repo.dart
@@ -0,0 +1,13 @@
+import '../../domain/repositories/init_group_repo.dart';
+import '../datasources/group_init_local_data_sources.dart';
+
+class GroupInitRepositoriesImp extends GroupInitRepositories {
+  const GroupInitRepositoriesImp(this.localDataSource);
+  final GroupInitLocalDataSource localDataSource;
+
+  @override
+  double getButtonPlace() => localDataSource.getFloatingPlace() ?? 0;
+
+  @override
+  Future<void> saveButtonPlace(double newTop) => localDataSource.saveFloatingPlace(newTop);
+}
---
+++ lib/features/chat/domain/repositories/init_group_repo.dart
@@ -0,0 +1,5 @@
+abstract class GroupInitRepositories {
+  const GroupInitRepositories();
+  double getButtonPlace();
+  Future<void> saveButtonPlace(double newTop);
+}
