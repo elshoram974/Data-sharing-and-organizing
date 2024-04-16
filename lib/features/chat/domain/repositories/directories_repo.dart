5e4fddd9ea2787e7ec32e782b0caa230c9b1d006
Mohammed Elshora <elshoram974@gmail.com>
2024-04-16 17:55:52 +0200

    Refactor directories repository implementation
    Create new directories repository implementation with improved structure and
    functionality. Update method signatures and add new method for retrieving
    directories.

--- lib/features/chat/domain/repositories/directories_repo.dart
+++ lib/features/chat/domain/repositories/directories_repo.dart
@@ -1,4 +1,9 @@
+import 'package:data_sharing_organizing/core/status/status.dart';
+
+import '../entities/directory_entity.dart';
+
 abstract class DirectoriesRepositories {
   const DirectoriesRepositories();
-  Future<void> saveBottomHeight(double height , int groupId);
+  Future<void> saveBottomHeight(double height, int groupId);
+  Stream<Status<List<DirectoryEntity>>> getDirectoriesInside([int? dirId]);
 }
