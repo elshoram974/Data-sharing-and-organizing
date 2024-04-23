78d0113e4434031b560aeae2ec16ed0aa707d4d5
Mohammed Elshora <elshoram974@gmail.com>
2024-04-23 22:13:57 +0200

    Refactor failureStatus function and add handleStatusEmit function
    Refactored the failureStatus function and added a new handleStatusEmit function
    to handle status emissions in a more structured way. This change improves code
    readability and maintainability.

--- lib/core/utils/functions/failure_status_dialog_emit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-
-void failureStatus(String error, void Function() emit) {
-  emit();
-  EasyLoading.showError(error, duration: const Duration(seconds: 5));
-}
---
+++ lib/core/utils/functions/handle_status_emit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+import '../../status/errors/failure.dart';
+import '../../status/status.dart';
+import '../../status/success/success.dart';
+
+void failureStatus(String error, void Function() emit) {
+  emit();
+  EasyLoading.showError(error, duration: const Duration(seconds: 5));
+}
+
+Future<void> handleStatusEmit<T>({
+  required final Future<Status<T>> Function() statusFunction,
+  required void Function(T data) successFunction,
+  void Function()? failureFunction,
+  bool dismissLoadingOnTap = false,
+}) async {
+  EasyLoading.show(dismissOnTap: dismissLoadingOnTap);
+  final Status<T> status = await statusFunction();
+  await EasyLoading.dismiss();
+  if (status is Success<T>) {
+    successFunction(status.data);
+  } else {
+    status as Failure<T>;
+    failureStatus(status.failure.message, failureFunction ?? () {});
+  }
+}
