71dc8c6648d5ace3ff2bf20573c84166ef5b33f4
Mohammed Elshora <elshoram974@gmail.com>
2024-04-30 16:57:03 +0300

    Add new UI components and functionality to AddMembersScreen
    Import new widgets and implement a floating action button, search bar, and member list in the AddMembersScreen.

--- lib/features/new_group/presentation/screens/add_members_screen.dart
+++ lib/features/new_group/presentation/screens/add_members_screen.dart
@@ -1,10 +1,49 @@
+import 'package:data_sharing_organizing/core/shared/member_list_tile/member_list_tile.dart';
+import 'package:data_sharing_organizing/core/shared/responsive/constrained_box.dart';
+import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_color.dart';
+import 'package:data_sharing_organizing/core/utils/entities/member_list_tile_entity.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/add_member/search_bar_members.dart';
+import '../widgets/new_group_app_bar.dart';
 
 class AddMembersScreen extends StatelessWidget {
   const AddMembersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColor.active,
+        child: const Icon(Icons.arrow_forward, color: Colors.white),
+      ),
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          NewGroupAppBar(title: S.of(context).addMembers),
+          SliverPersistentHeader(pinned: true, delegate: SearchBarMembers()),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 370,
+              (BuildContext context, int i) {
+                return ResConstrainedBoxAlign(
+                  child: MembersListTile(
+                    memberEntity: MemberListTileEntity(
+                      name: "Adel Eid -> ${i + 1}",
+                      imageLink: null,
+                      lastLogin: DateTime.now().subtract(Duration(days: i)),
+                      isSelected: false,
+                      onTileTapped: () {},
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
