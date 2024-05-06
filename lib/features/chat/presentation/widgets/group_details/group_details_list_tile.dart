023a448a1d697324d4ab82e761ad1f0e1c05aeee
Mohammed Elshora <elshoram974@gmail.com>
2024-05-06 17:13:56 +0300

    Refactor GroupDetailsListTile widget for improved flexibility
    Add color and padding properties to GroupDetailsListTile for more customization
    options. Refactor widget structure for better readability and maintainability.

--- lib/features/chat/presentation/widgets/group_details/group_details_list_tile.dart
+++ lib/features/chat/presentation/widgets/group_details/group_details_list_tile.dart
@@ -1,6 +1,5 @@
 import 'package:data_sharing_organizing/core/shared/responsive/constrained_box.dart';
 import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
-import 'package:data_sharing_organizing/core/utils/extension/padding_ex.dart';
 import 'package:data_sharing_organizing/core/utils/extension/responsive_ex.dart';
 import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +11,12 @@ class GroupDetailsListTile extends StatelessWidget {
     this.onTap,
     this.leading,
     this.trailing,
+    this.color,
+    this.padding,
   });
   final String title;
+  final Color? color;
+  final EdgeInsetsGeometry? padding;
   final Widget? leading;
   final Widget? trailing;
   final void Function()? onTap;
@@ -21,23 +24,31 @@ class GroupDetailsListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
-      child: ResConstrainedBoxAlign(
-        child: ListTile(
-          onTap: onTap,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: AppConst.defaultPadding),
-          shape: context.isPhoneWidth
-              ? const RoundedRectangleBorder(borderRadius: BorderRadius.zero)
-              : null,
-          title: Text(
-            title,
-            style: AppStyle.styleBoldInika24.copyWith(fontSize: 20),
+      child: Padding(
+        padding: padding ?? const EdgeInsets.only(top: 0.5 * AppConst.defaultPadding),
+        child: ResConstrainedBoxAlign(
+          child: ListTile(
+            onTap: onTap,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: AppConst.defaultPadding),
+            shape: context.isPhoneWidth
+                ? const RoundedRectangleBorder(borderRadius: BorderRadius.zero)
+                : null,
+            title: Text(
+              title,
+              style: AppStyle.styleBoldInika24.copyWith(
+                fontSize: 20,
+                color: color,
+              ),
+            ),
+            leadingAndTrailingTextStyle: AppStyle.styleBoldInika24.copyWith(
+              fontSize: 20,
+              color: color,
+            ),
+            leading: leading,
+            trailing: trailing,
           ),
-          leadingAndTrailingTextStyle:
-              AppStyle.styleBoldInika24.copyWith(fontSize: 20),
-          leading: leading,
-          trailing: trailing,
-        ).topPadding(0.5 * AppConst.defaultPadding),
+        ),
       ),
     );
   }
