33d77b4dc34b721f5deeba408113e57a29440d12
Mohammed Elshora <elshoram974@gmail.com>
2024-05-06 19:51:43 +0300

    Add MembersListGroupDetails widget for displaying group members in group details view
    This commit adds a new widget MembersListGroupDetails to display group members in
    the group details view. It includes functionality to edit members' menu based on
    admin status.

---
+++ lib/features/chat/presentation/widgets/group_details/members_list_group_details.dart
@@ -0,0 +1,79 @@
+import 'package:data_sharing_organizing/core/shared/member_list_tile/member_list_tile.dart';
+import 'package:data_sharing_organizing/core/shared/responsive/constrained_box.dart';
+import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
+import 'package:data_sharing_organizing/core/utils/entities/member_list_tile_entity.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../user_home/domain/entities/group_home_entity.dart';
+
+class MembersListGroupDetails extends StatelessWidget {
+  const MembersListGroupDetails({
+    super.key,
+    required this.list,
+    required this.group,
+  });
+
+  final Iterable list;
+  final GroupHomeEntity group;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: list.length,
+        (BuildContext context, int i) {
+          return ResConstrainedBoxAlign(
+            child: MembersListTile(
+              onTileTappedDown: group.memberEntity.isAdmin && i != 0
+                  ? (_) => editMembersMenu(context, _, i == 0)
+                  : null,
+              memberEntity: MemberListTileEntity(
+                name: "Adel Eid -> ${i + 1}",
+                isAdmin: i == 0 || i == 5 || i == 8,
+                imageLink:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOaoCj5ZMdS0xOCIYLMVT8ReRCVjCCzX01BPJH3MpsA&s',
+                lastLogin: DateTime.now().subtract(Duration(days: i)),
+                isSelected: false,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+void editMembersMenu(
+  BuildContext context,
+  TapDownDetails offset,
+  bool isAdmin,
+) {
+  final double x = offset.globalPosition.dx;
+  final double y = offset.globalPosition.dy;
+  final Size size = MediaQuery.sizeOf(context);
+  showMenu(
+    context: context,
+    position: RelativeRect.fromLTRB(x, y, size.width - x, size.height - y),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(AppConst.borderRadius),
+    ),
+    items: [
+      PopupMenuItem(
+        onTap: () {},
+        child: Text(S.of(context).remove),
+      ),
+      if (isAdmin)
+        PopupMenuItem(
+          onTap: () {},
+          child: Text(S.of(context).dismissAsAdmin),
+        ),
+      if (!isAdmin)
+        PopupMenuItem(
+          onTap: () {},
+          child: Text(S.of(context).promoteGroupAdmin),
+        ),
+    ],
+    elevation: 8.0,
+  );
+}
