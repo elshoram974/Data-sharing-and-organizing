6586bece98baaa4cab290acb29c8853c9f142499
Mohammed Elshora <elshoram974@gmail.com>
2024-05-06 17:14:18 +0300

    Add new widgets and update UI in GroupDetailsScreen
    Import new widgets and update UI elements in GroupDetailsScreen
    to display member count, add members tile, and exit group tile.

--- lib/features/chat/presentation/screens/group_details_screen.dart
+++ lib/features/chat/presentation/screens/group_details_screen.dart
@@ -1,7 +1,13 @@
+import 'package:data_sharing_organizing/core/shared/member_list_tile/member_list_tile.dart';
+import 'package:data_sharing_organizing/core/shared/responsive/constrained_box.dart';
+import 'package:data_sharing_organizing/core/utils/entities/member_list_tile_entity.dart';
 import 'package:flutter/material.dart';
 
+import '../../../new_group/presentation/widgets/add_group_details/members_count_widget.dart';
 import '../../../user_home/domain/entities/group_home_entity.dart';
+import '../widgets/group_details/add_members_tile_group_details.dart';
 import '../widgets/group_details/app_bar/app_bar_group_details.dart';
+import '../widgets/group_details/exit_group_tile_group_details.dart';
 import '../widgets/group_details/group_permissions_tile_group_details.dart';
 import '../widgets/group_details/media_docs_tile_group_details.dart';
 import '../widgets/group_details/mute_notifications_tile_group_details.dart';
@@ -13,6 +19,7 @@ class GroupDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List list = [5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22, 23];
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -20,16 +27,31 @@ class GroupDetailsScreen extends StatelessWidget {
           const MediaDocsTileGroupDetails(),
           const MuteNotificationsTileGroupDetails(),
           const GroupPermissionsTileGroupDetails(),
+          MembersCountWidget(
+            topPadding: 44,
+            bottomPadding: 0,
+            selectedUsers: list,
+          ),
+          if (group.memberEntity.isAdmin) const AddMembersTileGroupDetails(),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('Item $index'),
+              childCount: list.length >= 6 ? 6 : list.length,
+              (BuildContext context, int i) {
+                return ResConstrainedBoxAlign(
+                  child: MembersListTile(
+                    memberEntity: MemberListTileEntity(
+                      name: "Adel Eid -> ${i + 1}",
+                      imageLink:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOaoCj5ZMdS0xOCIYLMVT8ReRCVjCCzX01BPJH3MpsA&s',
+                      lastLogin: DateTime.now().subtract(Duration(days: i)),
+                      isSelected: false,
+                    ),
+                  ),
                 );
               },
-              childCount: 100,
             ),
           ),
+          const ExitGroupTileGroupDetails(),
         ],
       ),
     );
