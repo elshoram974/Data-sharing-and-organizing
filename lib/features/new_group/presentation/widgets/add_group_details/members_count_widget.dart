29f1ed2b9a41b2bb76ce5693f5824c09bee9aadf
Mohammed Elshora <elshoram974@gmail.com>
2024-05-06 17:14:04 +0300

    Refactor MembersCountWidget constructor for flexibility
    Add topPadding and bottomPadding parameters to allow customization of widget spacing. Update padding and text style accordingly.

--- lib/features/new_group/presentation/widgets/add_group_details/members_count_widget.dart
+++ lib/features/new_group/presentation/widgets/add_group_details/members_count_widget.dart
@@ -5,25 +5,36 @@ import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class MembersCountWidget extends StatelessWidget {
-  const MembersCountWidget({super.key, required this.selectedUsers});
+  const MembersCountWidget({
+    super.key,
+    required this.selectedUsers,
+    this.topPadding = 0,
+    this.bottomPadding = 15,
+  });
 
   final List selectedUsers;
+  final double topPadding;
+  final double bottomPadding;
 
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: Padding(
-        padding: const EdgeInsets.only(
+        padding: EdgeInsets.only(
           left: AppConst.defaultPadding,
           right: AppConst.defaultPadding,
-          bottom: 15,
+          top: topPadding,
+          bottom: bottomPadding,
         ),
         child: ResConstrainedBoxAlign(
           child: SizedBox(
             width: double.maxFinite,
             child: Text(
               S.of(context).membersCount(selectedUsers.length),
-              style: AppStyle.styleBoldInika16.copyWith(fontSize: 12),
+              style: AppStyle.styleBoldInika16.copyWith(
+                fontSize: 12,
+                color: Theme.of(context).textTheme.bodyLarge?.color,
+              ),
             ),
           ),
         ),
