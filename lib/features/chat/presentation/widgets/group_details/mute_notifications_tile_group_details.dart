a13adbdd48965a79b70a527a53c88bff639c4331
Mohammed Elshora <elshoram974@gmail.com>
2024-05-06 16:37:11 +0300

    Add ProviderDependency for mute status in group details tile widget
    Import ProviderDependency to access group mute status in the group details tile
    widget. Update the value property of the switch widget to use the group's mute
    status. Add a TODO comment for future changes related to muting in groups.

--- lib/features/chat/presentation/widgets/group_details/mute_notifications_tile_group_details.dart
+++ lib/features/chat/presentation/widgets/group_details/mute_notifications_tile_group_details.dart
@@ -1,5 +1,6 @@
 import 'package:data_sharing_organizing/core/shared/switch/custom_switch.dart';
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:flutter/material.dart';
 
 import 'group_details_list_tile.dart';
@@ -17,8 +18,8 @@ class MuteNotificationsTileGroupDetails extends StatelessWidget {
         color: Theme.of(context).textTheme.bodyLarge?.color,
       ),
       trailing: AbstractCustomSwitch(
-        value: true,
-        onChanged: (bool val) {},
+        value: ProviderDependency.group.group.isMute,
+        onChanged: (bool val) {}, // TODO: change mute in groups
       ),
     );
   }
