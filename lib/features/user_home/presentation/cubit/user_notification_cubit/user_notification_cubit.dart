adca89312d605f7677d1824817e6755069f52f86
Mohammed Elshora <elshoram974@gmail.com>
2024-05-18 02:13:04 +0300

    Refactor UserNotificationCubit to use repository for notifications
    This commit refactors the UserNotificationCubit to use the NotificationRepositories repository for getting and updating notifications instead of a hardcoded list. It also updates the onPressExpanded function to properly update the notification and emit the ExpandNotificationState.

--- lib/features/user_home/presentation/cubit/user_notification_cubit/user_notification_cubit.dart
+++ lib/features/user_home/presentation/cubit/user_notification_cubit/user_notification_cubit.dart
@@ -1,11 +1,6 @@
-import 'package:data_sharing_organizing/core/utils/enums/home/group_discussion_type_enum.dart';
-import 'package:data_sharing_organizing/core/utils/enums/message_type/message_type.dart';
-import 'package:data_sharing_organizing/core/utils/functions/sort_groups_by_last_activity_time.dart';
-import 'package:data_sharing_organizing/features/chat/domain/entities/activity_entity.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
-import '../../../../chat/domain/entities/member_entity.dart';
 import '../../../domain/entities/group_notification_entity.dart';
 import '../../../domain/repositories/notification_repositories.dart';
 
@@ -14,70 +9,23 @@ part 'user_notification_state.dart';
 class UserNotificationCubit extends Cubit<UserNotificationState> {
   final NotificationRepositories repo;
   UserNotificationCubit(this.repo) : super(const UserNotificationInitial()) {
-    list.sort(compareLastActivity);
-    currentNotifications.addAll(list);
+    currentNotifications.addAll(repo.getNotifications());
   }
   final List<GroupNotificationEntity> currentNotifications = [];
 
-  void onPressExpanded(GroupNotificationEntity notification) {
-    final GroupNotificationEntity replaced =
-        notification.copyWith(isExpanded: !notification.isExpanded);
+  void onPressExpanded(GroupNotificationEntity notification) async{
+    final GroupNotificationEntity replaced = notification.copyWith(
+      isExpanded: !notification.isExpanded,
+      isUnread: false,
+      unReadCounter: null,
+    );
 
-    final int index = currentNotifications.indexOf(replaced);
+    final int index = currentNotifications.indexWhere(
+      (e) => e.lastActivity?.id == replaced.lastActivity?.id,
+    );
+    await repo.updateNotifications(replaced);
     currentNotifications[index] = replaced;
 
     emit(ExpandNotificationState(replaced));
   }
 }
-
-final list = [
-  GroupNotificationEntity(
-      memberEntity:
-          MemberEntity.newEmpty().copyWith(joinDate: DateTime(2001, 2, 3)),
-      id: 0,
-      createdAt: DateTime(2001, 2, 3),
-      imageLink: 'https://images.justwatch.com/poster/248497985/s592/one-piece',
-      groupName: 'First year in THIET aa aa a a a a a a a a a a a a',
-      unReadCounter: 0,
-      ownerId: 42,
-      discussion: GroupDiscussionType.exist),
-  GroupNotificationEntity(
-    memberEntity: MemberEntity.newEmpty(),
-    id: 1,
-    createdAt: DateTime.now(),
-    lastActivity: ActivityEntity(
-      id: 1,
-      groupId: 1,
-      createdBy: MemberEntity.newEmpty(),
-      content:
-          'Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message ',
-      createdAt: DateTime.now(),
-      isApproved: true,
-      type: MessageType.photo,
-    ),
-    imageLink: '',
-    groupName: 'First year in THIET',
-    ownerId: 42,
-    discussion: GroupDiscussionType.existButClosed,
-  ),
-  GroupNotificationEntity(
-    memberEntity: MemberEntity.newEmpty(),
-    id: 2,
-    createdAt: DateTime.now(),
-    lastActivity: ActivityEntity(
-      id: 2,
-      groupId: 2,
-      createdBy: MemberEntity.newEmpty(),
-      content:
-          'Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message Last message ',
-      createdAt: DateTime.now(),
-      isApproved: true,
-      type: MessageType.voiceMessage,
-    ),
-    imageLink: '',
-    groupName: 'First year in THIET',
-    unReadCounter: 1,
-    ownerId: 42,
-    discussion: GroupDiscussionType.notExist,
-  ),
-];
