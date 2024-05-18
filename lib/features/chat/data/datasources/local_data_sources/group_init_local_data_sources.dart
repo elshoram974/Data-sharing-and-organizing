7c0a2cf1f66fe0ce8caa950afa56df79c0656d7b
Mohammed Elshora <elshoram974@gmail.com>
2024-05-19 00:19:26 +0300

    Add makeSeenToGroup method to GroupInitLocalDataSource
    This commit adds a new method called makeSeenToGroup to the GroupInitLocalDataSource abstract class. The implementation of this method in the GroupInitLocalDataSourceImp class makes use of the HomeLocalDataSource and NotificationLocalDataSource to mark a group as seen.

--- lib/features/chat/data/datasources/local_data_sources/group_init_local_data_sources.dart
+++ lib/features/chat/data/datasources/local_data_sources/group_init_local_data_sources.dart
@@ -1,10 +1,15 @@
 import 'package:data_sharing_organizing/core/utils/constants/app_strings.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/locator.dart';
 import 'package:data_sharing_organizing/core/utils/services/init_local.dart';
+
+import '../../../../user_home/data/datasources/home_datasources/home_local_data_sources.dart';
+import '../../../../user_home/data/datasources/home_datasources/notification_local_data_sources.dart';
 
 abstract class GroupInitLocalDataSource {
   const GroupInitLocalDataSource();
   double? getFloatingPlace();
   Future<void> saveFloatingPlace(double newTop);
+  Future<void> makeSeenToGroup(int groupId);
 }
 
 class GroupInitLocalDataSourceImp extends GroupInitLocalDataSource {
@@ -18,5 +23,13 @@ class GroupInitLocalDataSourceImp extends GroupInitLocalDataSource {
   @override
   Future<void> saveFloatingPlace(double newTop) {
     return config.put(AppStrings.floatingButtonPlace, '$newTop');
+  }
+
+  @override
+  Future<void> makeSeenToGroup(int groupId) {
+    return Future.wait([
+      sl.get<HomeLocalDataSource>().makeSeenToGroup(groupId),
+      sl.get<NotificationLocalDataSource>().makeSeenToGroup(groupId),
+    ]);
   }
 }
