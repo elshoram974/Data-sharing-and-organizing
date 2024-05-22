692c389b7dd7e019319f451a1c8a45e1f9641494
Mohammed Elshora <elshoram974@gmail.com>
2024-05-22 22:11:10 +0300

    Add media and docs count to group details tile in chat feature

--- lib/features/chat/presentation/widgets/group_details/media_docs_tile_group_details.dart
+++ lib/features/chat/presentation/widgets/group_details/media_docs_tile_group_details.dart
@@ -1,32 +1,57 @@
 import 'package:data_sharing_organizing/core/utils/config/locale/generated/l10n.dart';
+import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
+import 'package:data_sharing_organizing/core/utils/styles.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../user_home/domain/entities/group_home_entity.dart';
 import 'group_details_list_tile.dart';
 
-class MediaDocsTileGroupDetails extends StatelessWidget {
+class MediaDocsTileGroupDetails extends StatefulWidget {
   const MediaDocsTileGroupDetails({super.key});
+
+  @override
+  State<MediaDocsTileGroupDetails> createState() =>
+      _MediaDocsTileGroupDetailsState();
+}
+
+class _MediaDocsTileGroupDetailsState extends State<MediaDocsTileGroupDetails> {
+  final GroupHomeEntity group = ProviderDependency.group.group;
+  late final Reference _storageRef = FirebaseStorage.instance.ref();
+  late final String filesPath = "${group.groupId}/files";
+  late final Reference filesRef = _storageRef.child(filesPath);
+  ListResult? result;
+
+  @override
+  void initState() {
+    filesRef.listAll().then((val) => setState(() => result = val));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GroupDetailsListTile(
-      onTap: () {},
+      onTap: result == null ? null : () {},
       title: S.of(context).mediaAndDocs,
       trailing: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 100),
         child: Row(
           mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Flexible(
-              child: Center(
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  alignment: Alignment.topCenter,
-                  child: Text('321'),
+            Flexible(
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                alignment: Alignment.topCenter,
+                child: Text(
+                  '${result?.items.length ?? 0}',
+                  style: AppStyle.styleBoldInika16,
                 ),
               ),
             ),
+            const SizedBox(width: 0.5 * AppConst.defaultPadding),
             Icon(
               Icons.arrow_forward_ios_outlined,
               color: Theme.of(context).textTheme.bodyLarge?.color,
