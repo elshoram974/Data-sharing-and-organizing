d704b3d12e0420c02c6e342d5560ecfe45016aa3
Mohammed Elshora <elshoram974@gmail.com>
2024-06-19 11:16:45 +0300

    make searchMembers in remote data source in group details

--- lib/features/chat/data/datasources/remote_data_sources/group_details_remote_data_sources.dart
+++ lib/features/chat/data/datasources/remote_data_sources/group_details_remote_data_sources.dart
@@ -3,11 +3,15 @@ import 'package:data_sharing_organizing/core/utils/services/api_services.dart';
 
 import '../../models/group_details_members/group_details_members.dart';
 import '../../models/group_details_members/group_members_model.dart';
+import '../../models/search_member_model/search_member_model.dart';
+import '../../models/search_member_model/searched_member_model.dart';
 
 abstract class GroupDetailsRemoteDataSource {
   const GroupDetailsRemoteDataSource();
 
   Future<List<GroupMember>> getGroupMembers(int groupId);
+
+  Future<List<SearchedMemberModel>> searchMembers(String query);
 }
 
 class GroupDetailsRemoteDataSourceImp extends GroupDetailsRemoteDataSource {
@@ -22,5 +26,14 @@ class GroupDetailsRemoteDataSourceImp extends GroupDetailsRemoteDataSource {
       {'group_id': '$groupId'},
     );
     return GroupDetailsMembers.fromMap(response).members;
+  }
+
+  @override
+  Future<List<SearchedMemberModel>> searchMembers(String query) async {
+    Map<String, dynamic> response = await service.post(
+      AppLinks.groupMembers,
+      {'quere': query},
+    );
+    return SearchMemberModel.fromMap(response).searchedMembers;
   }
 }
