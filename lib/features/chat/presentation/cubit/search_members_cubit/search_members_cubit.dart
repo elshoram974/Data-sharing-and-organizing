668df5584307dbae151fba65948cf704baf0108e
Mohammed Elshora <elshoram974@gmail.com>
2024-06-19 11:49:29 +0300

    make searchMembers method and states in cubit

--- lib/features/chat/presentation/cubit/search_members_cubit/search_members_cubit.dart
+++ lib/features/chat/presentation/cubit/search_members_cubit/search_members_cubit.dart
@@ -1,10 +1,62 @@
+import 'package:data_sharing_organizing/core/status/errors/failure_body.dart';
+import 'package:data_sharing_organizing/core/utils/functions/handle_status_emit.dart';
+import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../data/models/group_details_members/group_members_model.dart';
+import '../../../data/models/search_member_model/searched_member_model.dart';
+import '../../../domain/repositories/group_details_repo.dart';
+import '../group_details/group_details_cubit.dart';
 
 part 'search_members_state.dart';
 
 abstract class SearchMembersCubit extends Cubit<SearchMembersState> {
-  SearchMembersCubit() : super(SearchMembersInitial());
+  SearchMembersCubit() : super(const SearchMembersInitial());
+
+  final GroupDetailsCubitImp detailsCubit = ProviderDependency.groupDetails;
+  late final GroupDetailsRepositories repo = detailsCubit.groupDetailsRepo;
+
+  List<SearchedMemberModel> currentSearched = [];
+
+  String query = " ";
+
+  void searchMembers();
 }
 
-class SearchMembersCubitImp extends SearchMembersCubit {}
+class SearchMembersCubitImp extends SearchMembersCubit {
+  SearchMembersCubitImp(){
+    searchMembers();
+  }
+
+  @override
+  void searchMembers() {
+    emit(const SearchMembersLoadingState());
+    handleStatusEmit<List<SearchedMemberModel>>(
+      statusFunction: () => repo.searchMembers(query),
+      successFunction: (_) {
+        final List<SearchedMemberModel> temp = [];
+
+        outer:
+        for (GroupMember e in detailsCubit.members) {
+          for (SearchedMemberModel s in _) {
+            if (s.userId == e.memberId) continue outer;
+          }
+          temp.add(
+            SearchedMemberModel(
+              userId: e.memberId,
+              firstName: e.firstName,
+              lastName: e.lastName,
+              lastLogin: e.lastLogin,
+              image: e.image,
+            ),
+          );
+        }
+
+        currentSearched = temp;
+        emit(SearchMembersSuccessState(currentSearched));
+      },
+      failureFunction: (f) => emit(SearchMembersFailureState(f)),
+    );
+  }
+}
--- lib/features/chat/presentation/cubit/search_members_cubit/search_members_state.dart
+++ lib/features/chat/presentation/cubit/search_members_cubit/search_members_state.dart
@@ -4,7 +4,29 @@ abstract class SearchMembersState extends Equatable {
   const SearchMembersState();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-class SearchMembersInitial extends SearchMembersState {}
+class SearchMembersInitial extends SearchMembersState {
+  const SearchMembersInitial();
+}
+
+class SearchMembersLoadingState extends SearchMembersState {
+  const SearchMembersLoadingState();
+}
+
+class SearchMembersSuccessState extends SearchMembersState {
+  const SearchMembersSuccessState(this.members);
+  final List<SearchedMemberModel> members;
+
+  @override
+  List<Object?> get props => [members];
+}
+
+class SearchMembersFailureState extends SearchMembersState {
+  const SearchMembersFailureState(this.failure);
+  final FailureBody failure;
+
+  @override
+  List<Object?> get props => [failure];
+}
