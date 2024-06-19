a9829954505ef73c1fd602ef96dbaf3c92ab36da
Mohammed Elshora <elshoram974@gmail.com>
2024-06-19 22:51:35 +0300

    can make update to ui when edit group data

--- lib/features/chat/presentation/widgets/group_title.dart
+++ lib/features/chat/presentation/widgets/group_title.dart
@@ -4,8 +4,10 @@ import 'package:data_sharing_organizing/core/utils/constants/app_constants.dart'
 import 'package:data_sharing_organizing/core/utils/services/dependency/provider_dependency.dart';
 import 'package:data_sharing_organizing/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../user_home/domain/entities/group_home_entity.dart';
+import '../cubit/group_cubit/group_cubit.dart';
 
 class GroupTitle extends StatelessWidget {
   const GroupTitle({
@@ -17,21 +19,26 @@ class GroupTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final GroupHomeEntity group = ProviderDependency.group.group;
-    return Row(
-      mainAxisAlignment: mainAxisAlignment,
-      children: [
-        CircularImageWidget(
-          imageLink: group.imageLink,
-          dimension: 42,
-          errorWidget: const GroupImage(),
-        ),
-        const SizedBox(width: .5 * AppConst.defaultPadding),
-        Text(
-          group.groupName,
-          style: AppStyle.styleBoldInika24.copyWith(fontSize: 20),
-        ),
-      ],
+    return BlocBuilder<GroupCubit, GroupState>(
+      buildWhen: (p, c) => c is GroupUpdateGroupDetails,
+      builder: (context, state) {
+        final GroupHomeEntity group = ProviderDependency.group.group;
+        return Row(
+          mainAxisAlignment: mainAxisAlignment,
+          children: [
+            CircularImageWidget(
+              imageLink: group.imageLink,
+              dimension: 42,
+              errorWidget: const GroupImage(),
+            ),
+            const SizedBox(width: .5 * AppConst.defaultPadding),
+            Text(
+              group.groupName,
+              style: AppStyle.styleBoldInika24.copyWith(fontSize: 20),
+            ),
+          ],
+        );
+      },
     );
   }
 }
