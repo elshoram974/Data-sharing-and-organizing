697e01980e81f7b557a1cac69f68bd7d29fa44e7
Mohammed Elshora <elshoram974@gmail.com>
2024-06-23 10:46:22 +0300

    cancel fn that save file in chat and depend on the prev fn

--- lib/core/utils/functions/handle_tapped_message.dart
+++ lib/core/utils/functions/handle_tapped_message.dart
@@ -1,15 +1,11 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-// import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:http/http.dart' as http;
 import 'package:open_filex/open_filex.dart';
-// import 'dart:html' as html;
 
 import 'package:path_provider/path_provider.dart';
-
-// import '../constants/app_constants.dart';
 
 Future<void> handleTappedMessage({
   required types.Message tappedMessage,
@@ -60,19 +56,3 @@ Future<void> handleTappedMessage({
     await OpenFilex.open(localPath);
   }
 }
-
-// Future<void> _saveFile(String fileName, Uint8List fileBytes) async {
-//   if (AppConst.isWeb) {
-//     // Web: Use AnchorElement to download the file
-//     final html.Blob blob = html.Blob([fileBytes]);
-//     final String url = html.Url.createObjectUrlFromBlob(blob);
-//     html.AnchorElement(href: url)
-//       ..setAttribute("download", fileName)
-//       ..click();
-//     html.Url.revokeObjectUrl(url);
-//   } else {
-//     // Android: Save to cache directory using flutter_cache_manager
-//     final DefaultCacheManager cacheManager = DefaultCacheManager();
-//     final File file = await cacheManager.putFile(fileName, fileBytes);
-//   }
-// }
